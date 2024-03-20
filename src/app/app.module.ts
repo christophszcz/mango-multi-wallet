55bfc3bae48f8bf6f6adbabca60e66a831653d2a
Chris Szczechowicz <info@cjszczechowicz.com>
2024-03-20 00:15:40 -0400

    Create the coin-table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CoinTableComponent } from './coin-table/coin-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ src/app/coin-table/coin-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-table',
+  templateUrl: './coin-table.component.html',
+  styleUrl: './coin-table.component.scss'
+})
+export class CoinTableComponent {
+
+}
